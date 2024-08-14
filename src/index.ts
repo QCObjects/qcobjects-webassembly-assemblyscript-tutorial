69ab501c1e83d8079dc9f523b725b33a060ebb52
Jean Machuca <correojean@gmail.com>
2024-08-13 20:18:16 -0400

    typescript

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import fs from "node:fs";
+import loader from "@assemblyscript/loader";
+import { shortCode } from "qcobjects";
+
+const imports = {
+  index: {
+    helloWorld ():void {
+      console.log(`Hello World! This is your key: ${shortCode()}`)
+    }
+  },
+  env: {
+    abort(_msg: any, _file: any, line: string, column: string):void {
+      console.error("abort called at main.ts:" + line + ":" + column);
+    }
+  },
+};
+const wasmModule = loader.instantiateSync(fs.readFileSync(__dirname + "/build/untouched.wasm"), imports as any);
+module.exports = wasmModule.exports;
